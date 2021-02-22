1fec233b588d53f6cd92b60a8e9f09a4363e23f9
Robert Hammock <rhamm1027@gmail.com>
2021-02-22 17:52:15 -0500

    greeting and style edit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,15 +7,35 @@ import './Header.css'
 class Header extends Component {
   static contextType = UserContext
 
+  state = {
+    greeting: '',
+  }
+
+  componentDidMount() {
+    const date = new Date();
+    const hr = date.getHours();
+    let greeting = '';
+    if (hr < 12) {
+      greeting = 'Good morning, ';
+    } else if (hr < 16) {
+      greeting = 'Good afternoon, ';
+    } else {
+      greeting = 'Good evening, ';
+    }
+    this.setState({ greeting });
+  }
+
   handleLogoutClick = () => {
     this.context.processLogout()
   }
 
   renderLogoutLink() {
+    const { greeting } = this.state;
+
     return (
       <div>
         <span>
-          {this.context.user.name}
+          {greeting}{this.context.user.name}
         </span>
         <nav>
           <Link
