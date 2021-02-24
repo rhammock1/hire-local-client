3c80b5511e4837da6aedacd0de2b1be1578f4fc8
Robert Hammock <rhamm1027@gmail.com>
2021-02-24 13:02:35 -0500

    Updated landing zip code info

--- src/components/LandingPopUp/LandingPopUp.js
+++ src/components/LandingPopUp/LandingPopUp.js
@@ -22,7 +22,7 @@ const LandingPopUp = function(props) {
         This app's goal is to help connect local businesses with local talent. Users can post new jobs and search for jobs. Users can apply for jobs by uploading a resume on sign up or on your account page. 
       </p>
       <p>
-        As a user you can search for jobs by title, zip code, and radius. All test jobs are labeled 'Test Title' and under the zipcode <strong>80014</strong>. A message 
+        As a user you can search for jobs by title, zip code, and radius. All test jobs are under the following zipcodes: <strong>80014, 28215, 33556</strong>. A message 
         will alert you if no jobs match your search.
       </p>
       <Button type="button" onClick={handleClear}>Clear</Button>
